3517e68c74b54ebc4baab43d9b628a8debe1bbea
Hector David <hectordavid55555211@gmail.com>
2025-02-26 20:14:22 -0500

    commit

---
+++ PYTHON/comienzo.py
@@ -0,0 +1,15 @@
+print('Hola mundo')
+mensaje="Bye mundo"
+print(mensaje)
+print(type(mensaje))
+flotante=3.5
+print(flotante)
+Boleano=False
+print(Boleano)
+
+edad:int=input("introduce la edad")
+edad=int(edad)
+print("")
+if edad >= 18:
+    print('viejaza')
+
