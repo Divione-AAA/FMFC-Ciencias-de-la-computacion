18555ce8bf4135145677adff3f83c1965f69f803
Hector David <hectordavid55555211@gmail.com>
2025-03-07 22:24:39 -0500

    commit

--- PYTHON/funciones.py
+++ PYTHON/funciones.py
@@ -10,3 +10,49 @@ def saludo(nombre, apellido):
 def saludo2(nombre="puto"):
     """Por omision"""
     return "Hola " + nombre
+
+#parametros arbitrarios
+def saludo3(nombre, *args):
+    """
+    Parametros arbitrarios, se utiliza isinstance para verificar tipo
+    es mejor que type porque verifica incluso si es una clase o un objeto q hereda
+    """
+    for arg in args:
+        if isinstance(arg,str):
+            return "Hola " + arg
+    return "Hola " + nombre
+
+print(saludo3("puto", "tico"))
+
+#paeametros arbitrarios tipo mapa (diccionario)
+def saludo4(nombre, **args):
+    """
+    ** significa que los parametros son de tipo mapa y args.items() es cada elemento
+    se recorre de forma clave valor sobre args.iems no sobre args solo
+    """
+    for clave,valor in args.items():
+        print(clave, ": ", valor)
+    return "Hola " + nombre
+
+print(saludo4("Lois",apellido="puto"))
+
+#datos de llamada como un arreglo
+
+def importe(saldo, interes):
+    """
+    Se puede pasar un arreglo como parametro
+    """
+    return saldo * interes
+
+TPAR = [1000,0.07]
+print(importe(*TPAR))
+
+#tmb como mapas
+TPAR = {"saldo":1000,"interes":0.08}
+print(importe(**TPAR))
+
+#notar como se usan * y **
+
+"""
+Las llamadas dinamicas
+"""
