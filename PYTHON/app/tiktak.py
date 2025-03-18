6f2eb37fa9e59cdb68d611ccb9bd7f98b1c17747
Hector David <hectordavid55555211@gmail.com>
2025-03-17 23:22:52 -0400

    commit

--- PYTHON/app/tiktak.py
+++ PYTHON/app/tiktak.py
@@ -5,10 +5,15 @@ Cruz y cero
 import tkinter as tk
 from tkinter import messagebox
 
+#debi ponerlo con mayusculas
+
 player = 'X'
 game_over = False
 
 def check():
+    """
+    una funcion para verificar si el juego ya ha terminado
+    """
     for i in range(3):
         if button[i][0]['text'] == button[i][1]['text'] == button[i][2]['text'] !='':
             return True
@@ -19,6 +24,7 @@ def check():
     return False
 
 def button_click(row,col):
+    """para manejar los clicks, la propiedad [text]"""
     global player, game_over
     if button[row][col]['text'] == '' and not game_over:
         button[row][col]['text'] = player
@@ -33,6 +39,7 @@ def button_click(row,col):
             player = '0' if player == 'X' else 'X'
 
 def reset():
+    """reseteo"""
     global player, game_over
     player = 'X'
     game_over = False
@@ -48,7 +55,7 @@ root.configure(bg='#263238')
 
 frame = tk.Frame(root, bg='#263238')
 frame.place(relx=0.5,rely=0.5,anchor='center')
-
+#arreglo de botones
 button =[[tk.Button(frame,text='',font='normal 20 bold',width=5,height=2,bg='#263238',fg='white',
         command = lambda row = row, col = col: button_click(row,col))
         for col in range(3)] for row in range(3)]
