aad5c6e41b3e5ae879178b29ebd3430c489f1b08
Hector David <hectordavid55555211@gmail.com>
2025-03-26 23:43:04 -0400

    commit

--- JAVA/Ejercicio 20/src/Climb.java
+++ JAVA/Ejercicio 20/src/Climb.java
@@ -8,4 +8,16 @@ public class Climb {
         ans[n]=answer(n+1,m) + answer(n+2,m);
         return ans[n];
     }
+
+    public int fib(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fib(n-1)+fib(n-2);
+    }
+
+    public int pow(int n,int p){
+        if(p==0) return 1;
+        if(p==1) return n;
+        return pow(n,p-1)*n;
+    }
 }
