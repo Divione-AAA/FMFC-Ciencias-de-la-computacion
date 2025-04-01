6abe21361222624414551dd53a396838dc7ffdcc
Hector David <hectordavid55555211@gmail.com>
2025-03-31 21:35:11 -0400

    commit

---
+++ SQL/prueba.sql
@@ -0,0 +1,13 @@
+-- Creando la base de datos
+
+CREATE DATABASE <Ejemplo>;
+
+CREATE TABLE User(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    status ENUM('ACTIVE', 'INACTIVE') DEFAULT 'INACTIVE',
+    username VARCHAR(50) UNIQUE NOT NULL,
+    mail VARCHAR(100) UNIQUE NOT NULL,
+    age INT UNSIGNED NOT NULL,
+    bio TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
