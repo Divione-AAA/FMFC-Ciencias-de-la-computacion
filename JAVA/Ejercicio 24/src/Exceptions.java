3f56662cee311de119e3c6d6a612fda4ae887c03
Hector David <hectordavid55555211@gmail.com>
2025-04-03 23:07:05 -0400

    commit

---
+++ JAVA/Ejercicio 24/src/Exceptions.java
@@ -0,0 +1,5 @@
+public class Exceptions extends Exception{
+    public Exceptions(String s){
+        super(s);
+    }
+}
---
+++ JAVA/Ejercicio 24/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exceptions{
+        /*int r=-1;
+        long m=-1;
+        Scanner cin = new Scanner(System.in);
+
+        try {
+            r = cin.nextInt();
+            m = Integer.toUnsignedLong(r);
+        }catch (Exception e){
+            e.printStackTrace();
+        }*/
+
+        ArrayList<Integer> j = new ArrayList<>();
+        int g = j.indexOf(4);
+        //System.out.println(g);
+
+        /*try {
+            int m = j.get(g);
+        }catch (Exception e){
+            e.printStackTrace();
+        }*/
+
+        /*if(g<0){
+            Exceptions e = new Exceptions("Not found");
+            throw e;
+        }*/
+    }
+}
