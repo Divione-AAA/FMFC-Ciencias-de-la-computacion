0d93cfacf44feb742028fbf8e92ea7ba27e1c13a
Hector David <hectordavid55555211@gmail.com>
2025-05-19 12:47:53 -0400

    commit

--- JAVA/Ejercicio 35/src/Main.java
+++ JAVA/Ejercicio 35/src/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         File archivo = new File("src/test.txt");
-        archivo.createNewFile();
 
+        //FileWriter fw = new FileWriter(archivo);
+        //fw.write("Love");
+        //fw.close();
+        BufferedReader br = new BufferedReader(new FileReader(archivo));
+        String linea;
+        while ((linea = br.readLine()) != null) {
+            System.out.println(linea);
+        }
+        br.close();
     }
 }
