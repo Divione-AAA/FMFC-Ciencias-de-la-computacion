7dcde2ebe41b283372331c7dc6eedf438906b36f
Hector David <hectordavid55555211@gmail.com>
2025-05-31 14:49:14 -0400

    commit

---
+++ JAVA/mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+import javax.swing.*;
+/**
+ *
+ * @author Hector David
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JFrame f = new NewJFrame();
+        f. setLocationRelativeTo(null);
+        f.setVisible(true);
+    }
+}
