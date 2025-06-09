f8ef9e8955ca7d3aa0cfe8a5fb68ed17f6b43a2c
Hector David <hectordavid55555211@gmail.com>
2025-06-09 13:38:17 -0400

    commit

---
+++ JAVA/Resumen/src/Ejemplo.java
@@ -0,0 +1,11 @@
+public class Ejemplo extends java.math.BigDecimal{
+    public transient int numero;
+
+    public void sumar(){
+        super.precision();
+    }
+
+    public Ejemplo(char[] in, int offset, int len) {
+        super(in, offset, len);
+    }
+}
