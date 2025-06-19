324cbe70706a0c15814dd5b1b5bd5a82e24478d5
Hector David <hectordavid55555211@gmail.com>
2025-06-19 10:59:49 -0400

    commit

--- ENSAMBLADOR/VS/primalidad.asm
+++ ENSAMBLADOR/VS/primalidad.asm
@@ -8,36 +8,50 @@ includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib 
 .stack
 .data
-origen dd 1,2,3,4,5,6,7,8,9,10
-destino dd  10 dup(?)
+origen  dd 1,2,3,4,5,6,7,8,9,10
+destino dd 10 dup(?)
 .code
 
-esprimo proc uses eax num:dword, arr:dword
-    mov ebx,2
-    mov eax,num
-    .repeat
-    div ebx
-    .if edx == 0 && ebx !=num
-    mov ecx,0
+esprimo proc uses eax ebx ecx num:dword
+    mov eax, num
+    cmp eax, 2
+    jl  no_primo      ; Menor que 2 no es primo
+    mov ecx, 2
+    mov ebx, eax
+    dec ebx           ; ebx = num-1
+check_loop:
+    cmp ecx, ebx
+    jg  es_primo
+    mov edx, 0
+    mov eax, num
+    div ecx
+    cmp edx, 0
+    je  no_primo
+    inc ecx
+    jmp check_loop
+es_primo:
+    mov eax, 1
     ret
-    .endif
-    inc ebx
-    .until ebx == num
-    mov ecx,1
+no_primo:
+    mov eax, 0
     ret
 esprimo endp
 
-primo proc uses eax tam:dword, arr:dword
-    xor esi,esi
-    .repeat
-    invoke esprimo, arr[esi*4], arr
-    mov destino[esi*4],ecx
+primo proc uses esi edi
+    mov esi, 0
+next:
+    cmp esi, 10
+    jge fin
+    push origen[esi*4]
+    call esprimo
+    mov destino[esi*4], eax
     inc esi
-    .until esi == tam
+    jmp next
+fin:
     ret
 primo endp
 
 start:
-    invoke primo, lengthof destino, destino
-invoke ExitProcess, 0
+    call primo
+    invoke ExitProcess, 0
 end start
