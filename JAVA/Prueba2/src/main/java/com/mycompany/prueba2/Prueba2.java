3154758043339e5b54777133269575420489ee01
Hector David <hectordavid55555211@gmail.com>
2025-06-29 00:07:37 -0400

    commit

---
+++ JAVA/Prueba2/src/main/java/com/mycompany/prueba2/Prueba2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba2;
+
+/**
+ *
+ * @author Hector David
+ */
+public class Prueba2 {
+
+    public static void main(String[] args) {
+        NewJFrame t = new NewJFrame();
+        t.setVisible(true);
+    }
+}
