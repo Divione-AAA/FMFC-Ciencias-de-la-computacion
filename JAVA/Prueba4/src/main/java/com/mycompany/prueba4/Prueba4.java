1c1fa8626dc931085bb3d139906f89e5c593c8ae
Hector David <hectordavid55555211@gmail.com>
2025-07-02 16:04:45 -0400

    commit

---
+++ JAVA/Prueba4/src/main/java/com/mycompany/prueba4/Prueba4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba4;
+
+/**
+ *
+ * @author Hector David
+ */
+public class Prueba4 {
+
+    public static void main(String[] args) {
+        NewJFrame t = new NewJFrame();
+        t.setVisible(true);
+    }
+}
