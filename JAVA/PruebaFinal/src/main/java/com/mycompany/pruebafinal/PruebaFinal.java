b1dc50a990a68307a76cecf4c4a2bf10856dc4c7
Hector David <hectordavid55555211@gmail.com>
2025-07-04 00:36:55 -0400

    commit

---
+++ JAVA/PruebaFinal/src/main/java/com/mycompany/pruebafinal/PruebaFinal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebafinal;
+
+/**
+ *
+ * @author Hector David
+ */
+public class PruebaFinal {
+
+    public static void main(String[] args) {
+        NewJFrame t = new NewJFrame();
+        t.setTitle("Datos del Paciente");
+        t.setVisible(true);
+    }
+}
