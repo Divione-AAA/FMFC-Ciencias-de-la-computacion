1e4aa7d8663cf40a8e3f382cea32f33ebe2e359a
Hector David <hectordavid55555211@gmail.com>
2025-07-09 21:19:55 -0400

    commit

---
+++ C/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int contador;
+char f;
+double t;
+float h;
+
+
+int main() {
+    printf("Hola mundo\n");
+    scanf("%d",&contador);
+    scanf("%c",&f);
+    scanf("%f",t);
+    scanf("%f",&h);
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
