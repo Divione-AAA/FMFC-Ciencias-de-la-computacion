9d2b1618edad9b37d0e5e144aba5acfa0a48c62d
Hector David <hectordavid55555211@gmail.com>
2025-08-04 11:08:26 -0400

    commit

--- SQL/t1.sql
+++ SQL/t1.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE t1;
 
 CREATE TABLE Autores(
-    id INT NOT NULL AUTO_INCREMENT 
-    nombre VARCHAR(50) NOT NULL 
-    apellidos VARCHAR(50) NOT NULL
-    telefono VARCHAR(50) NOT NULL
-    direccion VARCHAR(150) NOT NULL
-    ciudad VARCHAR(50) NOT NULL
+    id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    telefono VARCHAR(50) NOT NULL,
+    direccion VARCHAR(150) NOT NULL,
+    ciudad VARCHAR(50) NOT NULL,
     estado VARCHAR(20) NOT NULL
 )
 
