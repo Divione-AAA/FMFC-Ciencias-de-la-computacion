c941adcca038345d65ee920d8872c5601e6c039f
Hector David <hectordavid55555211@gmail.com>
2025-08-15 23:58:06 -0400

    commit

--- ICPC2/bellman ford/bigmac.cpp
+++ ICPC2/bellman ford/bigmac.cpp
@@ -28,40 +28,43 @@ struct edge{
     double w;
 };
 
-int n,m,v,a,b,t1,t2;
-double t3;
+int n,m,a,b,t1,t2;
+double t3,v;
 vector<edge> adj;
 
 double bellman(){
-    vector<double> d(n+1,INF);
+    vector<double> d(n,INF);
     d[a]=0;
-    for (int i=0; i<n-1; i++){
+    for (int i=0; i<n; i++){
         for(auto e: adj){
-            if (d[e.u] != INF && d[e.u] + e.w < d[e.v])
-                d[e.v] = d[e.u] + e.w;
+            if (d[e.u] != INF)
+                d[e.v] = min(d[e.u] + e.w,d[e.v]);
         }
     }
-
-    return d[b];
-}
+    for (auto &e : adj) {
+        if (d[e.u] != INF && d[e.u] + e.w < d[e.v]) {
+            return 0; 
+        }
+    }
+return exp(d[b]);}
 
 int32_t main(){
 	
 	fast
 	//freopen("file.in", "r", stdin);freopen("file.out", "w", stdout)
     
-    cin>>n>>m>>v>>a>>b;
-    adj.resize(n+1);
+    cin>>n>>m>>v>>a>>b;a--,b--;
+    adj.resize(n);
 
     loop(i,m){
         cin>>t1>>t2>>t3;
-        adj.PB({t1,t2,t3});
+        t1--,t2--;
+        adj.PB({t1,t2,log(t3)});
         
     }
-
-    //cout<<"hello"<<endl;
+    
     double ans = bellman();
     if(ans==0){cout<<0; return 0;}
-    else cout<<fixed<<setprecision(2)<<(ans*v)<<endl;
+    else cout<<fixed<<setprecision(2)<<(ans*v);
 
 return 0;}
