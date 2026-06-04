57e1913cd72b556cd99269bc930fb3d0fb6bbadb
Hector David <hectordavid55555211@gmail.com>
2026-06-04 09:22:22 -0400

    commit

---
+++ ESTRUCTURA DE DATOS 2/heap.java
@@ -0,0 +1,12 @@
+public interface PriorityQueue<E> {
+
+    void insert(E element, double priority);
+
+    E extractMax() throws EmptyPriorityQueueException;
+
+    E getMax() throws EmptyPriorityQueueException;
+
+    boolean isEmpty();
+
+    int size();
+}
